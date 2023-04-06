d6c5dd1b1d8c3df6ff0bc1286d2d72212a18e6e6
lecs01 <lecs.01@outlook.com>
2023-04-06 11:59:41 -0300

    Criacao da classe Medico

--- src/desenvolvimento/Medico.java
+++ src/desenvolvimento/Medico.java
@@ -1,4 +1,37 @@
 package desenvolvimento;
 
+import java.util.Scanner;
+
 public class Medico {
+    private int CRM;
+    private String nome;
+
+    public int getCRM() {
+        return CRM;
+    }
+
+    public void setCRM(int CRM) {
+        this.CRM = CRM;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void cadastrar() {
+        Scanner cad = new Scanner(System.in);
+        System.out.println("Nome: ");
+        this.setNome(cad.nextLine());
+        System.out.println("CRM: ");
+        this.setCRM(cad.nextInt());
+    }
+
+    public void imprimirMedico() {
+        System.out.println("Médico: " + this.getNome());
+        System.out.println("CRM: " + this.getCRM());
+    }
 }
